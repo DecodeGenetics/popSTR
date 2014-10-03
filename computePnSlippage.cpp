1979db8220a9927f630075a831c6dbf6676a3071
Snaedis Kristmundsdottir <snaedis.kristmundsdottir@decode.is>
2014-10-03 13:55:56 +0000

    computePnSlippage computes a slippage rate over entire genome for a specific PN

---
+++ computePnSlippage.cpp
@@ -0,0 +1,117 @@
+#include <iostream>
+#include <set>
+#include <map>
+#include <string>
+#include <math.h>
+#include <algorithm>
+#include <fstream>
+#include <seqan/sequence.h>
+#include <seqan/basic.h>
+#include <seqan/file.h>
+#include <seqan/modifier.h>
+
+using namespace std;
+using namespace seqan;
+
+//For storing number of members in each class and their pValue-sum
+struct LabelProps {
+    double p1;
+    double p2;
+    double p3;
+} ;
+
+void appendChrAndPnId(CharString& dir, string chromNum, string pnId)
+{
+    append(dir,"chr");
+    append(dir,chromNum);
+    append(dir,"/");
+    append(dir,pnId);
+}
+
+int main(int argc, char const ** argv)
+{   
+    //Check arguments.
+    if (argc != 3)
+    {
+        cerr << "USAGE: " << argv[0] << " directoryToAttributesAndLabels PN-id";
+        return 1;
+    }
+        
+    CharString attDir = argv[1];
+    append(attDir, "/attributes/");
+    CharString labDir = argv[1];
+    append(labDir, "/initialLabels/");
+    CharString currAttDir, currLabDir;
+    string pnId = argv[2];
+    string temp, chrId;
+    int numberOfReads;
+    float winner, second, numOfRepeats;
+    LabelProps slippCount;
+    slippCount.p1 = 0;
+    slippCount.p2 = 0;
+    slippCount.p3 = 0;
+    double slippage;
+    
+    for (unsigned i=1; i<25; ++i)
+    {
+        if (i<23)
+        {
+            stringstream ss;
+            ss << i;
+            chrId = ss.str();
+        }
+        if (i == 23)
+            chrId = "X";
+        if (i == 24)
+            chrId = "Y";
+        currAttDir = attDir;
+        currLabDir = labDir;
+        appendChrAndPnId(currAttDir, chrId, pnId);
+        appendChrAndPnId(currLabDir, chrId, pnId);
+        append(currAttDir, "attributes");
+        append(currLabDir, "initialLabels");
+        ifstream attributeFile(toCString(currAttDir));
+        ifstream initialLabels(toCString(currLabDir));
+        attributeFile >> pnId;
+        while (!attributeFile.eof() && !initialLabels.eof())
+        {            
+            attributeFile >> temp;
+            attributeFile >> temp;        
+            attributeFile >> temp;
+            attributeFile >> temp;
+            attributeFile >> temp;
+            attributeFile >> numberOfReads;
+            attributeFile >> temp;
+            initialLabels >> winner;
+            initialLabels >> second;
+            for (unsigned i = 0; i < numberOfReads; ++i)
+            {
+                attributeFile >> numOfRepeats;
+                attributeFile >> temp;
+                attributeFile >> temp;
+                attributeFile >> temp;
+                attributeFile >> temp;
+                attributeFile >> temp;
+                attributeFile >> temp;
+                attributeFile >> temp;
+                attributeFile >> temp;
+                attributeFile >> temp;
+                attributeFile >> temp;
+                if (numOfRepeats == winner || numOfRepeats == second)
+                    slippCount.p1 += 0.95;
+                else 
+                {
+                    if ((numOfRepeats == winner - 1) || (numOfRepeats == second - 1))
+                        slippCount.p2 += 0.95;
+                    else
+                        slippCount.p3 += 0.95;
+                }
+            }
+        }
+        attributeFile.close();
+        initialLabels.close();   
+    }
+    slippage = (slippCount.p2 + slippCount.p3)/(2.0*(slippCount.p1 + slippCount.p2 + slippCount.p3));
+    cout << "Slippage for " << pnId << " is: " << slippage << endl;
+    return 0;    
+}
