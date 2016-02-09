79c00259d5358c3637f6600ed4886b96cefb2861
Snaedis Kristmundsdottir <snaedis.kristmundsdottir@decode.is>
2016-02-09 16:20:36 +0000

    Adding fraction computation to output and monitoring super allele function.

--- compareAtts.py
+++ compareAtts.py
@@ -9,7 +9,7 @@ if len(argv) < 2:
     
 old_attribute_file = argv[1]
 new_attribute_file = argv[2]
-posToAlleles = defaultdict(list)
+posToAlleles = defaultdict(set)
 nDiffAlleles = 0
 newAlleles = 0
 nMoreReads = 0
@@ -19,6 +19,7 @@ nNewMarkers = 0
 nCompMarkers = 0
 nLostAlleles = 0
 nSameAlleles = 0
+nSuperAlleles = 0
 
 with open(old_attribute_file, 'r') as old_atts_file:
     pn = old_atts_file.readline()
@@ -29,7 +30,7 @@ with open(old_attribute_file, 'r') as old_atts_file:
             chrom,begin,end,motif,refrep,nreads,refseq = line.split()
             for i in range(0, int(nreads)):
                 l = old_atts_file.readline()
-                posToAlleles[begin].append(l.split()[0])                
+                posToAlleles[begin].add(l.split()[0])                
             line = old_atts_file.readline()
         else:
             print 'Something is wrong!'
@@ -44,14 +45,20 @@ with open(new_attribute_file, 'r') as new_atts_file:
         if line[0:3] == 'chr':
             nNewMarkers += 1
             chrom,begin,end,motif,refrep,nreads,refseq,a1,a2 = line.split()
+            superAllele = False 
             if begin in posToAlleles: 
                 nCompMarkers += 1
-                oldAlleleSet = set(posToAlleles[begin])                  
+                oldAlleleSet = posToAlleles[begin]                  
                 for i in range(0, int(nreads)):
                     l = new_atts_file.readline()
-                    if l.split()[0] not in oldAlleleSet:
+                    allele = l.split()[0]
+                    if allele not in oldAlleleSet:
                         newAlleles += 1
-                    newAlleleSet.add(l.split()[0])                                                
+                    newAlleleSet.add(allele)
+                    if abs(float(allele)*len(motif) - 70) < 0.05:
+                        superAllele = True
+                if superAllele:
+                    nSuperAlleles +=1                                             
                 if int(nreads) > len(posToAlleles[begin]):
                     nMoreReads += 1
                     #if len(newAlleleSet.difference(oldAlleleSet))>0:
@@ -86,4 +93,7 @@ print 'Number of markers with more reads in old file: ' + str(nLessReads) + ''
 #print 'Number of markers where I lose alleles in new version: ' + str(nLostAlleles) + ''
 print 'Number of markers with new alleles: ' + str(nDiffAlleles) + ''
 print 'Number of markers with same alleles in both versions: ' + str(nSameAlleles) + ''
+print 'Fraction of markers with new alleles: ' + str(float(nDiffAlleles)/nCompMarkers)
+print 'Fraction of markers with same alleles: ' + str(float(nSameAlleles)/nCompMarkers)
+print 'Number of markers with super alleles: ' + str(nSuperAlleles)
             
