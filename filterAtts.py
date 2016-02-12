0f3b0b17e6386eeddd65fd18aef85155fa009954
Snaedis Kristmundsdottir <snaedis.kristmundsdottir@decode.is>
2016-02-12 12:54:52 +0000

    Changing parameters so user can specify directory to write output to.

--- filterAtts.py
+++ filterAtts.py
@@ -2,12 +2,13 @@
 
 from sys import argv
 
-if len(argv) < 3:
-    print 'Usage: ' + argv[0] + ' <filtered markers file> <attributes file 1> [ ... <attributes file N> ]'
+if len(argv) < 4:
+    print 'Usage: ' + argv[0] + ' <filtered markers file> <outputDir> <attributes file 1> [ ... <attributes file N> ]'
     exit(1)
     
 filtered_markers_file = argv[1]
-attributes_files = argv[2:]
+out_directory = argv[2]
+attributes_files = argv[3:]
 
 filtered_markers = []
 with open(filtered_markers_file, 'r') as fmfile:
@@ -18,9 +19,9 @@ filtered_markers = set(filtered_markers)
 
 for attrfile in attributes_files:
     with open(attrfile, 'r') as infile:
-        with open(attrfile + 'filtered', 'w') as outfile:
-            pn = infile.readline()
-            outfile.write(pn)
+        pn = infile.readline().split()[0]
+        with open(out_directory + pn + '', 'w') as outfile:
+            outfile.write(pn + '\n')
             line = infile.readline()
             while line != '':
                 if line[0:3] != 'chr':
