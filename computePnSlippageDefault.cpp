3d5986a8c8f20991af8419bdbb0250d7b304f5b0
Snaedis Kristmundsdottir <snaedis.kristmundsdottir@decode.is>
2016-10-21 12:13:36 +0000

    Changing paths in liblinear include statements to match name of zip file.

--- computePnSlippageDefault.cpp
+++ computePnSlippageDefault.cpp
@@ -12,16 +12,16 @@
 #include <seqan/modifier.h>
 #include <seqan/stream.h>
 #include <seqan/arg_parse.h>
-#include <liblinear/linear.h>
-#include <liblinear/linear.cpp>
-#include <liblinear/tron.h>
-#include <liblinear/tron.cpp>
-#include <liblinear/blas/blas.h>
-#include <liblinear/blas/blasp.h>
-#include <liblinear/blas/daxpy.c>
-#include <liblinear/blas/ddot.c>
-#include <liblinear/blas/dnrm2.c>
-#include <liblinear/blas/dscal.c>
+#include <liblinear-2.01/linear.h>
+#include <liblinear-2.01/linear.cpp>
+#include <liblinear-2.01/tron.h>
+#include <liblinear-2.01/tron.cpp>
+#include <liblinear-2.01/blas/blas.h>
+#include <liblinear-2.01/blas/blasp.h>
+#include <liblinear-2.01/blas/daxpy.c>
+#include <liblinear-2.01/blas/ddot.c>
+#include <liblinear-2.01/blas/dnrm2.c>
+#include <liblinear-2.01/blas/dscal.c>
 
 using namespace std;
 using namespace seqan;
