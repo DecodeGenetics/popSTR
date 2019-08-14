aa49855faef551100f9621f9e352b84336d8f696
Snaedis Kristmundsdottir <snaedis.kristmundsdottir@decode.is>
2019-08-14 13:08:01 +0000

    Adding markers on chr1,chr8 and chr10 to panel.

--- checkExpansions.sh
+++ checkExpansions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 BAM=$1
 MaxRepLength=$2
-for i in {2,3,4,5,6,7,9,12,13,14,16,18,19,20,21,22}
+for i in {1,2,3,4,5,6,7,8,9,10,12,13,14,16,18,19,20,21,22}
 do
     ./computeReadAttributes $BAM . ./panelMarkerInfo/chr${i}markerInfo 4 $MaxRepLength chr${i}
 done
