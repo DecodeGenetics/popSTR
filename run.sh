9a6a961464bf203d27c2611d48d1698cbe6d4c74
Snaedis Kristmundsdottir <snaedis.kristmundsdottir@decode.is>
2019-08-30 10:33:50 +0000

    Adding variables to simplify running outside of code directory.

--- run.sh
+++ run.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 BAMLIST=$1
 REFERENCE=$2
+CODE_DIR=`dirname $0`
+CURR_DIR=`pwd`
+
 #run computeReadAttributes for all chromosomes in parallel
 echo "Computing read attributes."
 for i in {1..22}
 do
-    echo "computeReadAttributes ${BAMLIST} . markerInfo/chr${i}markerInfo 8 135 chr${i} ${REFERENCE}"
+    echo "${CODE_DIR}/computeReadAttributes ${BAMLIST} ${CURR_DIR} ${CODE_DIR}/markerInfo/chr${i}markerInfo 8 135 chr${i} ${REFERENCE}"
 done | parallel
 
 #run computePnSlippageDefault to get pnSlipps using kernel
 echo "Computing pn-slippage rates."
-computePnSlippageDefault -PL <(cols 1 $BAMLIST) -AD ./attributes/chr21 -OF pnSlippage -FP 1 -MS ./kernel/kernelSlippageRates -MD ./kernel/kernelModels
+${CODE_DIR}/computePnSlippageDefault -PL <(cols 1 $BAMLIST) -AD ${CURR_DIR}/attributes/chr21 -OF pnSlippage -FP 1 -MS ${CODE_DIR}/kernel/kernelSlippageRates -MD ${CODE_DIR}/kernel/kernelModels
 
 #make directory for vcf files
 echo "Making directory for vcf file."
@@ -20,5 +23,5 @@ mkdir -p vcfs
 echo "Genotyping markers."
 for i in {1..22}
 do
-    echo "msGenotyperDefault -ADCN ./attributes/chr${i} -PNS pnSlippage -MS markerSlippageChr${i} -VD ./vcfs -VN chr${i} -ML <(cols 1,2,3,4 markerInfo/chr${i}markerInfo) -I 0 -FP 1"
+    echo "${CODE_DIR}/msGenotyperDefault -ADCN ${CURR_DIR}/attributes/chr${i} -PNS pnSlippage -MS markerSlippageChr${i} -VD ./vcfs -VN chr${i} -ML <(cols 1,2,3,4 ${CODE_DIR}/markerInfo/chr${i}markerInfo) -I 0 -FP 1"
 done | parallel
--- runSmall.sh
+++ runSmall.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 BAMLIST=$1
 REFERENCE=$2
+CODE_DIR=`dirname $0`
 
 #Compute attributes for chr21 markers (from kernel)
-echo "computeReadAttributes ${BAMLIST} . ./kernel/kernelMarkersInfo 8 135 chr21 ${REFERENCE}"
-computeReadAttributes ${BAMLIST} . ./kernel/kernelMarkersInfo 8 135 chr21 ${REFERENCE}
+echo "${CODE_DIR}/computeReadAttributes ${BAMLIST} . ${CODE_DIR}/kernel/kernelMarkersInfo 8 135 chr21 ${REFERENCE}"
+${CODE_DIR}/computeReadAttributes ${BAMLIST} . ${CODE_DIR}/kernel/kernelMarkersInfo 8 135 chr21 ${REFERENCE}
 
 #Compute pnSlippage for samples in BAMLIST
-echo "computePnSlippageDefault -PL <(cols 1 ${BAMLIST}) -AD ./attributes/chr21 -OF pnSlippage -FP 1 -MS ./kernel/kernelSlippageRates -MD ./kernel/kernelModels"
-computePnSlippageDefault -PL <(cols 1 ${BAMLIST}) -AD ./attributes/chr21 -OF pnSlippage -FP 1 -MS ./kernel/kernelSlippageRates -MD ./kernel/kernelModels
+echo "${CODE_DIR}/computePnSlippageDefault -PL <(cols 1 ${BAMLIST}) -AD ./attributes/chr21 -OF pnSlippage -FP 1 -MS ${CODE_DIR}/kernel/kernelSlippageRates -MD ${CODE_DIR}/kernel/kernelModels"
+${CODE_DIR}/computePnSlippageDefault -PL <(cols 1 ${BAMLIST}) -AD ./attributes/chr21 -OF pnSlippage -FP 1 -MS ${CODE_DIR}/kernel/kernelSlippageRates -MD ${CODE_DIR}/kernel/kernelModels
 
 #make directory for vcf files
 echo "mkdir -p vcfs"
 mkdir -p vcfs
 
 #run msGenotyper
-echo "msGenotyperDefault -ADCN ./attributes/chr21 -PNS pnSlippage -MS markerSlippageChr21 -VD ./vcfs -VN chr21_small -ML <(cols 1,2,3,4 markerInfo/chr21markerInfo) -I 0 -FP 1"
-msGenotyperDefault -ADCN ./attributes/chr21 -PNS pnSlippage -MS markerSlippageChr21 -VD ./vcfs -VN chr21_small -ML <(cols 1,2,3,4 ./kernel/kernelMarkersInfo) -I 0 -FP 1
+echo "${CODE_DIR}/msGenotyperDefault -ADCN ./attributes/chr21 -PNS pnSlippage -MS markerSlippageChr21 -VD ./vcfs -VN chr21_small -ML <(cols 1,2,3,4 ${CODE_DIR}/kernel/kernelMarkersInfo) -I 0 -FP 1"
+${CODE_DIR}/msGenotyperDefault -ADCN ./attributes/chr21 -PNS pnSlippage -MS markerSlippageChr21 -VD ./vcfs -VN chr21_small -ML <(cols 1,2,3,4 ${CODE_DIR}/kernel/kernelMarkersInfo) -I 0 -FP 1
