d6981a358a7359f2e678204dbefb65948693f1c5
Snaedis Kristmundsdottir <snaedis.kristmundsdottir@decode.is>
2019-10-22 13:14:00 +0000

    Running last piece of markers in parallel with others instead of last. Checking for existence of pnSlippage file before recomputing. Fixing markerSlippageFile name error.

--- runPerChrom.sh
+++ runPerChrom.sh
@@ -11,30 +11,36 @@ N_JOBS=`calc ${TOTAL_MARKERS}/${MARKERS_PER_JOB} | cut -d ' ' -f 3 | awk '{print
 lastJobIdx=`calc ${N_JOBS}+1 | cut -d ' ' -f 3 | cut -d '.' -f 1`
 echo "Chromosome will be split into ${lastJobIdx} runs."
 
-#run computeReadAttributes, each batch of markers at a time
-echo "Computing read attributes."
-for ((i=1; i<=$N_JOBS; i++))
-do
-    headNum=`calc ${i}*${MARKERS_PER_JOB} | cut -d ' ' -f 3 | cut -d '.' -f 1`
-    echo "${CODE_DIR}/computeReadAttributes ${BAMLIST} ${CURR_DIR} <(head -n ${headNum} ${CODE_DIR}/markerInfo/${CHROM}markerInfo | tail -n ${MARKERS_PER_JOB}) 8 135 ${CHROM} ${REFERENCE} ${CODE_DIR}/markerInfo/longRepeats N"
-done | parallel
-
 nDone=`calc ${N_JOBS}*${MARKERS_PER_JOB} | cut -d ' ' -f 3 | cut -d '.' -f 1`
 tailNum=`calc ${TOTAL_MARKERS}-${nDone} | cut -d ' ' -f 3 | cut -d '.' -f 1`
-echo "Computing attributes for last ${tailNum} markers"
-echo "${CODE_DIR}/computeReadAttributes ${BAMLIST} ${CURR_DIR} <(tail -n ${tailNum} ${CODE_DIR}/markerInfo/${CHROM}markerInfo) 8 135 ${CHROM} ${REFERENCE} ${CODE_DIR}/markerInfo/longRepeats N"
-${CODE_DIR}/computeReadAttributes ${BAMLIST} ${CURR_DIR} <(tail -n ${tailNum} ${CODE_DIR}/markerInfo/${CHROM}markerInfo) 8 135 ${CHROM} ${REFERENCE} ${CODE_DIR}/markerInfo/longRepeats N
+
+#run computeReadAttributes, each batch of markers at a time
+echo "Computing read attributes."
+for ((i=1; i<=$lastJobIdx; i++))
+do
+    if [ $i -eq $lastJobIdx ]
+    then
+        echo "${CODE_DIR}/computeReadAttributes ${BAMLIST} ${CURR_DIR} <(tail -n ${tailNum} ${CODE_DIR}/markerInfo/${CHROM}markerInfo | cut -d ' ' -f 1-11,14-) 8 135 ${CHROM} ${REFERENCE} ${CODE_DIR}/markerInfo/longRepeats N"
+    else
+        headNum=`calc ${i}*${MARKERS_PER_JOB} | cut -d ' ' -f 3 | cut -d '.' -f 1`
+        echo "${CODE_DIR}/computeReadAttributes ${BAMLIST} ${CURR_DIR} <(head -n ${headNum} ${CODE_DIR}/markerInfo/${CHROM}markerInfo | tail -n ${MARKERS_PER_JOB} | cut -d ' ' -f 1-11,14-) 8 135 ${CHROM} ${REFERENCE} ${CODE_DIR}/markerInfo/longRepeats N"
+    fi
+done | parallel
 
 #Check if attributes for kernel markers have been computed
 chr21dir=${CURR_DIR}/attributes/chr21
 if [ ! -d "$chr21dir" ]; then
     echo "Attributes have not been computed for kernel markers and will be computed now."
-    ${CODE_DIR}/computeReadAttributes ${BAMLIST} ${CURR_DIR} ${CODE_DIR}/kernel/kernelMarkersInfo 8 135 chr21 ${REFERENCE} ${CODE_DIR}/markerInfo/longRepeats N
+    ${CODE_DIR}/computeReadAttributes ${BAMLIST} ${CURR_DIR} <(cut -d ' ' -f 1-11,14- ${CODE_DIR}/kernel/kernelMarkersInfo) 8 135 chr21 ${REFERENCE} ${CODE_DIR}/markerInfo/longRepeats N
 fi
 
-#run computePnSlippageDefault to get pnSlipps using kernel
-echo "Computing pn-slippage rates."
-${CODE_DIR}/computePnSlippageDefault -PL <(awk '{print $1}' $BAMLIST) -AD ${CURR_DIR}/attributes/chr21 -OF pnSlippage -FP 1 -MS ${CODE_DIR}/kernel/kernelSlippageRates -MD ${CODE_DIR}/kernel/kernelModels
+#check if pnSlippage has been computed
+pnSlippageFile=${CURR_DIR}/pnSlippage
+if [ ! -f "$pnSlippageFile" ]; then
+    echo "pn-slippage rates have not been estimated and will be estimated now."
+    ${CODE_DIR}/computePnSlippageDefault -PL <(awk '{print $1}' $BAMLIST) -AD ${CURR_DIR}/attributes/chr21 -OF pnSlippage -FP 1 -MS ${CODE_DIR}/kernel/kernelSlippageRates -MD ${CODE_DIR}/kernel/kernelModels
+fi
+
 
 #make directory for vcf files
 echo "Making directory for vcf file."
@@ -42,18 +48,19 @@ mkdir -p vcfs
 
 #run msGenotyper on all chromosomese using attributes and pnSlippage we now have
 echo "Genotyping markers."
-for ((i=1; i<=$N_JOBS; i++))
+for ((i=1; i<=$lastJobIdx; i++))
 do
-    headNum=`calc ${i}*${MARKERS_PER_JOB} | cut -d ' ' -f 3 | cut -d '.' -f 1`
-    echo "${CODE_DIR}/msGenotyperDefault -ADCN ${CURR_DIR}/attributes/${CHROM} -PNS pnSlippage -MS markerSlippage${CHROM} -VD ${CURR_DIR}/vcfs -VN ${CHROM} -ML <(cut -f 1,2,3,4 ${CODE_DIR}/markerInfo/${CHROM}markerInfo | head -n ${headNum} | tail -n ${MARKERS_PER_JOB}) -I ${i} -FP 1"
+    if [ $i -eq $lastJobIdx ]
+    then
+        echo "${CODE_DIR}/msGenotyperDefault -ADCN ${CURR_DIR}/attributes/${CHROM} -PNS pnSlippage -MS markerSlippage${CHROM} -VD ${CURR_DIR}/vcfs -VN ${CHROM} -ML <(cut -d ' ' -f 1,2,3,4,12,13 ${CODE_DIR}/markerInfo/${CHROM}markerInfo | tail -n ${tailNum}) -I ${lastJobIdx} -FP 1"
+    else
+        headNum=`calc ${i}*${MARKERS_PER_JOB} | cut -d ' ' -f 3 | cut -d '.' -f 1`
+        echo "${CODE_DIR}/msGenotyperDefault -ADCN ${CURR_DIR}/attributes/${CHROM} -PNS pnSlippage -MS markerSlippage${CHROM} -VD ${CURR_DIR}/vcfs -VN ${CHROM} -ML <(cut -d ' ' -f 1,2,3,4,12,13 ${CODE_DIR}/markerInfo/${CHROM}markerInfo | head -n ${headNum} | tail -n ${MARKERS_PER_JOB}) -I ${i} -FP 1"
+    fi
 done | parallel
 
-echo "Genotyping attributes for last ${tailNum} markers"
-echo "${CODE_DIR}/msGenotyperDefault -ADCN ${CURR_DIR}/attributes/${CHROM} -PNS pnSlippage -MS markerSlippage${CHROM} -VD ${CURR_DIR}/vcfs -VN ${CHROM} -ML <(cut -f 1,2,3,4 ${CODE_DIR}/markerInfo/${CHROM}markerInfo | tail -n ${tailNum}) -I ${lastJobIdx} -FP 1"
-${CODE_DIR}/msGenotyperDefault -ADCN ${CURR_DIR}/attributes/${CHROM} -PNS pnSlippage -MS markerSlippage${CHROM} -VD ${CURR_DIR}/vcfs -VN ${CHROM} -ML <(cut -f 1,2,3,4 ${CODE_DIR}/markerInfo/${CHROM}markerInfo | tail -n ${tailNum}) -I ${lastJobIdx} -FP 1
-
-#merge bam files
-echo "Merging BAM files"
+#merge vcf files
+echo "Merging vcf files"
 grep ^# ${CURR_DIR}/vcfs/${CHROM}_1.vcf > ${CURR_DIR}/vcfs/${CHROM}.vcf
 nHeaderLines=`wc -l ${CURR_DIR}/vcfs/${CHROM}.vcf | cut -d ' ' -f 1`
 ((nHeaderLines++))
@@ -71,6 +78,6 @@ tabix ${CURR_DIR}/vcfs/${CHROM}.vcf.gz
 echo "Merging markerSlippageFiles"
 for ((i=1; i<=$lastJobIdx; i++))
 do
-    cat markerSlippagechr21_${i} >> markerSlippagechr21
-    rm markerSlippagechr21_${i}
+    cat markerSlippage${CHROM}_${i} >> markerSlippage${CHROM}
+    rm markerSlippage${CHROM}_${i}
 done
